8be022f7b407d44458acb5ca8994afc8aeab9b14
Głogowski Szymon <94491671+simmo97@users.noreply.github.com>
2022-01-26 21:33:46 +0100

    Update ComM.c

--- Test_LinSM_Init/src/ComM.c
+++ Test_LinSM_Init/src/ComM.c
@@ -4,8 +4,7 @@
 STATIC NetworkHandleType Channel_mock;
 STATIC ComM_ModeType ComMode_mock;
 
-void ComM_BusSM_ModeIndication(NetworkHandleType Channel,ComM_ModeType ComMode)
-{
+void ComM_BusSM_ModeIndication(NetworkHandleType Channel,ComM_ModeType ComMode){
     Channel_mock = Channel;
     ComMode_mock = ComMode;
 }
@@ -13,23 +12,19 @@ void ComM_BusSM_ModeIndication(NetworkHandleType Channel,ComM_ModeType ComMode)
 
 #ifdef TEST_FUNCTIONS
 
-NetworkHandleType GetComM_BusSM_ModeIndicationChannel(void)
-{
+NetworkHandleType GetComM_BusSM_ModeIndicationChannel(void){
     return Channel_mock;
 }
 
-ComM_ModeType GetComM_BusSM_ModeIndicationComMode(void)
-{
+ComM_ModeType GetComM_BusSM_ModeIndicationComMode(void){
     return ComMode_mock;
 }
 
-void SetComM_BusSM_ModeIndicationChannel(NetworkHandleType Channel)
-{
+void SetComM_BusSM_ModeIndicationChannel(NetworkHandleType Channel){
      Channel_mock = Channel;
 }
 
-void SetComM_BusSM_ModeIndicationComMode(ComM_ModeType mode)
-{
+void SetComM_BusSM_ModeIndicationComMode(ComM_ModeType mode){
     ComMode_mock = mode;
 }
 
