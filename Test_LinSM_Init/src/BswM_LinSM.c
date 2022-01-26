7ef85a25e345d29a8b077c380c2ca5b1baec4417
Głogowski Szymon <94491671+simmo97@users.noreply.github.com>
2022-01-26 21:33:27 +0100

    Update BswM_LinSM.c

--- Test_LinSM_Init/src/BswM_LinSM.c
+++ Test_LinSM_Init/src/BswM_LinSM.c
@@ -3,14 +3,12 @@
 static NetworkHandleType Network_mock;
 static LinIf_SchHandleType CurrentSchedule_mock;
 
-void BswM_LinSM_CurrentSchedule(NetworkHandleType Network, LinIf_SchHandleType CurrentSchedule)
-{
+void BswM_LinSM_CurrentSchedule(NetworkHandleType Network, LinIf_SchHandleType CurrentSchedule){
     Network_mock = Network;
     CurrentSchedule_mock = CurrentSchedule;
 }
 
-void BswM_LinSM_CurrentState(NetworkHandleType Network, LinSM_ModeType CurrentState)
-{
+void BswM_LinSM_CurrentState(NetworkHandleType Network, LinSM_ModeType CurrentState){
     (void)Network;
     (void)CurrentState;
 }
@@ -18,13 +16,11 @@ void BswM_LinSM_CurrentState(NetworkHandleType Network, LinSM_ModeType CurrentSt
 
 #ifdef TEST_FUNCTIONS
 
-NetworkHandleType GetBswM_LinSM_CurrentScheduleNetwork(void)
-{
+NetworkHandleType GetBswM_LinSM_CurrentScheduleNetwork(void){
     return Network_mock;
 }
 
-LinSM_ModeType GetBswM_LinSM_CurrentSchedule(void)
-{
+LinSM_ModeType GetBswM_LinSM_CurrentSchedule(void){
     return CurrentSchedule_mock;
 }
 
